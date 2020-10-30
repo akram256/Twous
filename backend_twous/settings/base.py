a823f4255664bccc635fbbaa3328244b6a29a823
akram256 <mukasaakram55@gmail.com>
2020-10-30 23:52:50 +0300

    work on the error

--- backend_twous/settings/base.py
+++ backend_twous/settings/base.py
@@ -99,22 +99,22 @@ USE_TZ = True
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-# DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-         'TEST': {
-            'NAME': 'test twous'
-        }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#          'TEST': {
+#             'NAME': 'test twous'
+#         }
 
-    }
-}
+#     }
+# }
 
 
 # Password validation
